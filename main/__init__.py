77ce04a16666b705a0a6cf2ea35281891e690a14
Lullaby330 <69coder@gmail.com>
2023-10-02 13:48:56 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("6216349", default=None, cast=int)
+API_HASH = config("5c7418e9f3df6db931caa7354521c55f", default=None)
+BOT_TOKEN = config("6614032202:AAEtY2CZRXX0GD-EgP2KxXc_eKBv3t7XL_A", default=None)
+SESSION = config("AQBJMNojMub-86MHKkemO3LqWzra79oLIPXgYeGsQ3yl3rk0wnmB9STk_--ETseV_xlmpVyshizF9Sj5p-IaByYpmXrLZ4JBSQ1Iwa_ep_efwBEyVKBNHD8o61iXDeHZ7mf7OBbyvznYFeAwQr5d8XSySOh2ATnEXc2yHyk4CdkpZZF2EZHWJzYVGt6aAv3nghsVWiXPCpptbFPCseWnKur0waFCisAC9PHEhfnLDJE6rXpQ0flbT7_mDXCWtX87mPQ7H1K4rvVVT_wr5T4_pK5DcilPOELuoumcYPysjK7C84PKov_eTPhVXD-CHvnK6dXdrArGhkKUf_qDgUJhgWDWAAAAAYtBvCAA", default=None)
+FORCESUB = config("svcntn", default=None)
+AUTH = config("6631308320", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
